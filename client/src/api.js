8d58bc7150efeae14dd627731d9c9744644b3597
Omkar Jadhav <omkarjadhav3777@gmail.com>
2026-03-30 11:12:40 +0530

    fix vercel routing

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://portfolio-zwya.onrender.com"
+  baseURL: "https://portfolio-zwya.onrender.com/api"
 });
 
 export default API;
